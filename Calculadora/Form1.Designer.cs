780246022b3ac71d05d1584d9711cb775379a6a8
sebafebre <sebafebre5@gmail.com>
2024-07-07 16:42:42 -0300

    creacion del proyecto

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelBotones = new Panel();
+            panelPantalla = new Panel();
+            SuspendLayout();
+            // 
+            // panelBotones
+            // 
+            panelBotones.Dock = DockStyle.Left;
+            panelBotones.Location = new Point(0, 0);
+            panelBotones.Name = "panelBotones";
+            panelBotones.Size = new Size(195, 497);
+            panelBotones.TabIndex = 0;
+            // 
+            // panelPantalla
+            // 
+            panelPantalla.Dock = DockStyle.Fill;
+            panelPantalla.Location = new Point(0, 0);
+            panelPantalla.Name = "panelPantalla";
+            panelPantalla.Size = new Size(1006, 497);
+            panelPantalla.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1006, 497);
+            Controls.Add(panelBotones);
+            Controls.Add(panelPantalla);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelBotones;
+        private Panel panelPantalla;
+    }
+}
---
+++ Calculadora/MM2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class MM2 : Form
+    {
+        public MM2()
+        {
+            InitializeComponent();
+        }
+    }
+}
